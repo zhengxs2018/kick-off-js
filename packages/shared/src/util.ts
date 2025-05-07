8accee700f65d8238ecb5b4c88eff18c2a4b8431
zhenxgs2018 <zhengxs2018@gmail.com>
2025-05-07 13:56:31 +0800

    feat(shared): 添加 hasOwn 函数

--- packages/shared/src/util.ts
+++ packages/shared/src/util.ts
@@ -5,3 +5,12 @@ export function arrayify<T>(arr: T | T[]): T[] {
 export function isNil(value: unknown): value is null | undefined {
   return value === undefined || value === null
 }
+
+const hasOwnProperty = Object.prototype.hasOwnProperty
+
+export function hasOwn<T extends object, K extends PropertyKey = PropertyKey>(
+  o: unknown,
+  prop: K
+): o is T & Record<K, unknown> {
+  return isNil(o) == false && hasOwnProperty.call(o, prop)
+}
